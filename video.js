720604c113429185d332daede8cb3f4bc4027778
cinna-zju <cinna@yangshuodeMacBook-Air.local>
2017-07-12 11:06:20 -0400

    Autoplay

--- video.js
+++ video.js
@@ -1,32 +1,25 @@
 var detector = null;
+
 $(document).ready(function(){
 
   // var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
   var faceMode = affdex.FaceDetectorMode.SMALL_FACES;
   
   var detector = new affdex.FrameDetector(faceMode);
+
+  v = document.getElementById("video1");
+
     
   //Enable detection of all Expressions, Emotions and Emojis classifiers.
   detector.detectAllEmotions();
-  detector.detectAllExpressions();
+  // detector.detectAllExpressions();
   //detector.detectAllEmojis();
   //detector.detectAllAppearance();
+
   log("#logs","starting now...");
   var startTimestamp;
   detector.start();
 
-  //Add a callback to notify when the detector is initialized and ready for runing.
-  detector.addEventListener("onInitializeSuccess", function() {
-    log("#logs","started");
-    //startTimestamp = (new Date()).getTime() / 1000;
-    localStorage.clear();    
-  });
-
-  detector.addEventListener("onInitializeFailure", function () {
-    log("#logs","init failed");
-  });
-
-  v = document.getElementById("video1");
   v.addEventListener('play', function () {
     startTimestamp = (new Date()).getTime() / 1000;
     var i = window.setInterval(function () {
@@ -41,6 +34,39 @@ $(document).ready(function(){
     }, 200);
     
   }, false);
+  
+    var folder = 2; 
+    var no = 2;
+
+    v.addEventListener('ended', function () {
+      //detector.stop();
+      detector.reset();
+
+      // detector.start();
+      nextVideo();
+      localStorage.clear();
+      v.play();
+    })
+
+  //Add a callback to notify when the detector is initialized and ready for runing.
+  detector.addEventListener("onInitializeSuccess", function() {
+    log("#logs", "started");
+    v.play();
+    //startTimestamp = (new Date()).getTime() / 1000;    
+    
+    
+  });
+
+  detector.addEventListener("onInitializeFailure", function () {
+    log("#logs", "init failed, restarting now...");
+    // if ( detector.isRunning ) {
+    //   detector.stop();
+    //   // detector.reset();
+    //   // detector.start();
+    // }
+  });
+
+
 
   var cnt = 0;
   detector.addEventListener("onImageResultsSuccess", function (faces, image, timestamp) {  
@@ -48,13 +74,13 @@ $(document).ready(function(){
     log('#results', "Timestamp: " + timestamp.toFixed(2));
     log('#results', "Number of faces found: " + faces.length);
     if (faces.length > 0) {
-      log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
+      //log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
       log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
         return val.toFixed ? Number(val.toFixed(0)) : val;
       }));
-      log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
-       return val.toFixed ? Number(val.toFixed(0)) : val;
-      }));
+      // log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
+      //  return val.toFixed ? Number(val.toFixed(0)) : val;
+      // }));
       log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
       // log('#results', "Points: " + JSON.stringify(faces[0].featurePoints, function(key, val) {
       //     return val.toFixed ? Number(val.toFixed(0)) : val;
@@ -65,10 +91,9 @@ $(document).ready(function(){
         'time': timestamp.toFixed(2),
         'nums': 1,
         'emotion': faces[0].emotions,
-        'expression': faces[0].expressions
+        //'expression': faces[0].expressions
       }
-      localStorage.setItem(cnt, JSON.stringify(jsonData));
-      cnt = cnt + 1;
+
     } else {
       jsonData = {
         'time': timestamp.toFixed(2),
@@ -76,22 +101,22 @@ $(document).ready(function(){
         'emotion': {
           "joy": 0, "sadness": 0, "disgust": 0, "contempt": 0, "anger": 0,
           "fear": 0, "surprise": 0, "valence": 0, "engagement": 0
-        },
-        'expression': {
-          "smile": 0, "innerBrowRaise": 0, "browRaise": 0, "browFurrow": 0, "noseWrinkle": 0,
-          "upperLipRaise": 0, "lipCornerDepressor": 0, "chinRaise": 0, "lipPucker": 0, "lipPress": 0,
-          "lipSuck": 0, "mouthOpen": 0, "smirk": 0, "eyeClosure": 0, "attention": 0,
-          "lidTighten": 0, "jawDrop": 0, "dimpler": 0, "eyeWiden": 0, "cheekRaise": 0,
-          "lipStretch": 0
         }
+        // 'expression': {
+        //   "smile": 0, "innerBrowRaise": 0, "browRaise": 0, "browFurrow": 0, "noseWrinkle": 0,
+        //   "upperLipRaise": 0, "lipCornerDepressor": 0, "chinRaise": 0, "lipPucker": 0, "lipPress": 0,
+        //   "lipSuck": 0, "mouthOpen": 0, "smirk": 0, "eyeClosure": 0, "attention": 0,
+        //   "lidTighten": 0, "jawDrop": 0, "dimpler": 0, "eyeWiden": 0, "cheekRaise": 0,
+        //   "lipStretch": 0
+        // }
       
-      }
-      localStorage.setItem(cnt, JSON.stringify(jsonData));
-      cnt = cnt + 1;
+      } 
     }
+    localStorage.setItem(cnt, JSON.stringify(jsonData));
+    cnt = cnt + 1;
   });
 
-  //Draw the detected facial feature points on the image
+  //Draw the detected facial feature points on the image  
   function drawFeaturePoints(img, featurePoints) {
     var contxt = $('#canvas1')[0].getContext('2d');
 
@@ -108,9 +133,28 @@ $(document).ready(function(){
 
     }
   }
+
+  function nextVideo() {
+    $("#video1").attr("src", "./hci/Sessions/"+folder.toString()
+      + '/' + no.toString() + ".mp4");
+    
+    $("#logs").append("<span>" + "./hci/Sessions/"+folder.toString()
+      +'/'+no.toString()+".mp4" + "</span><br />");
+    
+    if(no == 4){
+      no = 0;
+      folder += 1;
+    }
+    if(folder > 2){
+      alert("done");
+    }
+    no += 1;
+  
+  }
 });
 
 function log(node_name, msg) {
-  $(node_name).append("<span>" + msg + "</span><br />")
+   $(node_name).append("<span>" + msg + "</span><br />") 
 }
+
 
